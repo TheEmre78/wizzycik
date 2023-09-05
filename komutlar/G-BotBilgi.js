6117d6b5d8c1bae7d1a51da49e12b54f9e9aeda1
Glitch (shadowrise) <none>
2023-09-05 09:25:22 +0000

    🐚🍇 Checkpoint ./komutlar/G-BotBilgi.js:55196954/113

--- komutlar/G-BotBilgi.js
+++ komutlar/G-BotBilgi.js
@@ -5,13 +5,13 @@ const ayarlar = require("../ayarlar.json");
 let prefix = ayarlar.prefix;
 
   const botbilgi = new Discord.MessageEmbed()
-    .setAuthor(`Windes | Discord Bot Bilgi Tablosu`)
+    .setAuthor(`CSL | Discord Bot Bilgi Tablosu`)
     .setTitle(``)
     .setColor("#00ff00")
     .setImage('https://cdn.discordapp.com/attachments/1108819464524415097/1109034877774483466/standard_7.gif')
     .setThumbnail(client.user.avatarURL())
     .setDescription(
-      `📯 Windes Botumuzu Eklemek İçin \`${prefix}davet\` yazabilirsiniz.`
+      `📯 CSL Botumuzu Eklemek İçin \`${prefix}davet\` yazabilirsiniz.`
     )
     .addField(
       "__**Bot Verileri**__",
@@ -25,7 +25,7 @@ let prefix = ayarlar.prefix;
     )
     .addField(
       "__**Yetkili Bilgi**__",
-      "👑 **Bot Sahipleri**  <@689447667465453599> \n\n"
+      "👑 **Bot Sahipleri**  <@1123251351552544919> \n\n"
     )
     .addField(
       "__**Sürümler**__",
@@ -38,7 +38,7 @@ let prefix = ayarlar.prefix;
     )
     .addField(
       `__Bilgilendirme__`,
-      `🎓  \`${prefix}davet\` | Botu Sununuya Davet Edersiniz\n 🎓 \`${prefix}botbilgi\` | Botun İstatistiklerini Görürsünüz.`
+      ` 🎓 \`${prefix}botbilgi\` | Botun İstatistiklerini Görürsünüz.`
     ); 
   
   return message.channel.send(botbilgi);
